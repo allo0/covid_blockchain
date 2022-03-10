f5a215e1bcdfa725e4bc7257d62867d3047c760c
allo0 <ias.topalidis@gmail.com>
2022-03-10 22:23:48 +0200

    (v2) Changed avg and max values in case of null

--- src/main/java/protal/bcserver/data/controller/CovidDataController.java
+++ src/main/java/protal/bcserver/data/controller/CovidDataController.java
@@ -145,12 +145,12 @@ public class CovidDataController {
             filtered_max_deaths = filtered_list_year_month.stream().mapToInt(c -> c.getDeaths()).max();
 
             deaths_obj.put("sum", filtered_sum_deaths);
-            deaths_obj.put("avg", avg_deaths.isPresent() ? avg_deaths.getAsDouble() : "0");
-            deaths_obj.put("max", filtered_max_deaths.isPresent() ? filtered_max_deaths.getAsInt() : "0");
+            deaths_obj.put("avg", avg_deaths.isPresent() ? avg_deaths.getAsDouble() : 0);
+            deaths_obj.put("max", filtered_max_deaths.isPresent() ? filtered_max_deaths.getAsInt() : 0);
             response_json.put("deaths", deaths_obj);
             logger.info("Sum deaths (all months): " + filtered_sum_deaths);
-            logger.info("Avg deaths: " + (avg_deaths.isPresent() ? avg_deaths.getAsDouble() : "0"));
-            logger.info("Max deaths (month with highest death rate): " + (filtered_max_deaths.isPresent() ? filtered_max_deaths.getAsInt() : "0"));
+            logger.info("Avg deaths: " + (avg_deaths.isPresent() ? avg_deaths.getAsDouble() : 0));
+            logger.info("Max deaths (month with highest death rate): " + (filtered_max_deaths.isPresent() ? filtered_max_deaths.getAsInt() : 0));
         }
 
         /**
@@ -170,12 +170,12 @@ public class CovidDataController {
             filtered_max_recovered = filtered_list_year_month.stream().mapToInt(c -> c.getRecovered()).max();
 
             recovered_obj.put("sum", filtered_sum_recovered);
-            recovered_obj.put("avg", avg_recovered.isPresent() ? avg_recovered.getAsDouble() : "0");
-            recovered_obj.put("max", filtered_max_recovered.isPresent() ? filtered_max_recovered.getAsInt() : "0");
+            recovered_obj.put("avg", avg_recovered.isPresent() ? avg_recovered.getAsDouble() : 0);
+            recovered_obj.put("max", filtered_max_recovered.isPresent() ? filtered_max_recovered.getAsInt() : 0);
             response_json.put("recovered", recovered_obj);
             logger.info("Sum recovered (all months): " + filtered_sum_recovered);
-            logger.info("Avg recovered: " + (avg_recovered.isPresent() ? avg_recovered.getAsDouble() : "0"));
-            logger.info("Max recovered (month with highest recovery rate): " + (filtered_max_recovered.isPresent() ? filtered_max_recovered.getAsInt() : "0"));
+            logger.info("Avg recovered: " + (avg_recovered.isPresent() ? avg_recovered.getAsDouble() : 0));
+            logger.info("Max recovered (month with highest recovery rate): " + (filtered_max_recovered.isPresent() ? filtered_max_recovered.getAsInt() : 0));
         }
 
         /**
@@ -195,12 +195,12 @@ public class CovidDataController {
             filtered_max_confirmed = filtered_list_year_month.stream().mapToInt(c -> c.getRecovered()).max();
 
             confirmed_obj.put("sum", filtered_sum_confirmed);
-            confirmed_obj.put("avg", avg_confirmed.isPresent() ? avg_confirmed.getAsDouble() : "0");
-            confirmed_obj.put("max", filtered_max_confirmed.isPresent() ? filtered_max_confirmed.getAsInt() : "0");
+            confirmed_obj.put("avg", avg_confirmed.isPresent() ? avg_confirmed.getAsDouble() : 0);
+            confirmed_obj.put("max", filtered_max_confirmed.isPresent() ? filtered_max_confirmed.getAsInt() : 0);
             response_json.put("confirmed", confirmed_obj);
             logger.info("Sum confirmed (all months): " + filtered_sum_confirmed);
-            logger.info("Avg confirmed: " + (avg_confirmed.isPresent() ? avg_confirmed.getAsDouble() : "0"));
-            logger.info("Max confirmed (month with highest confirmed infection rate): " + (filtered_max_confirmed.isPresent() ? filtered_max_confirmed.getAsInt() : "0"));
+            logger.info("Avg confirmed: " + (avg_confirmed.isPresent() ? avg_confirmed.getAsDouble() : 0));
+            logger.info("Max confirmed (month with highest confirmed infection rate): " + (filtered_max_confirmed.isPresent() ? filtered_max_confirmed.getAsInt() : 0));
         }
 
 
